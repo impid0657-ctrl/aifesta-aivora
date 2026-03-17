24e245ef5d9667619fc0a1eafbd7908c4275db3c
impid0657-ctrl <impid0657@icloud.com>
2026-03-17 11:57:55 +0900

    style: hero overlay 50% to 60%

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -52,7 +52,7 @@ export default function HeroSection({ hideFeatures = false, hideButtons = false,
               objectFit: 'cover', zIndex: 0,
             }}
           />
-          <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', background: 'rgba(0,0,0,0.5)', zIndex: 0 }} />
+          <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', background: 'rgba(0,0,0,0.6)', zIndex: 0 }} />
         </>
       )}
       <div className="container" style={{ position: 'relative', zIndex: 1 }}>
